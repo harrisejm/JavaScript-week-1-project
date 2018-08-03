7fb2a4a2f58ba15d128434e58979dc64e80e0f92
Eddie Harris <harrisejm@gmail.com>
2018-08-03 09:13:32 -0700

    add initial files

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+//import { age } from './age';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './styles.css';
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'project',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  plugins: [
+    new CleanWebpackPlugin(['dist']),   // new line
+    new HtmlWebpackPlugin({
+      title: 'project',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: "eslint-loader"
+      }
+    ]
+  }
+};
