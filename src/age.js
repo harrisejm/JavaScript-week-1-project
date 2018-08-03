372194984e864e28ba01f634e400f5d21cc9a75c
Eddie Harris <harrisejm@gmail.com>
2018-08-03 10:12:10 -0700

    round down for all outputed ages in Age class

--- src/age.js
+++ src/age.js
@@ -9,19 +9,19 @@ export class Age {
     return ageEarth;
   }
   mercuryAge() {
-    const ageMercury = this.earthAge() / .24;
+    const ageMercury = Math.floor(this.earthAge() / .24);
     return ageMercury;
   }
   venusAge() {
-    const ageVenus = this.earthAge() / .62;
+    const ageVenus = Math.floor(this.earthAge() / .62);
     return ageVenus;
   }
   marsAge() {
-    const ageMars = this.earthAge() * 1.88;
+    const ageMars = Math.floor(this.earthAge() * 1.88);
     return ageMars;
   }
   jupiterAge() {
-    const ageJupiter = this.earthAge() * 11.86;
+    const ageJupiter = Math.floor(this.earthAge() * 11.86);
     return ageJupiter;
   }
 
